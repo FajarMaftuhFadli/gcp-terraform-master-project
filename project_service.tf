93463bce992d754bd2a761cd0cb0c803a8da146d
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-26 11:38:37 +0700

    enable serviceusage.googleapis.com service

--- project_service.tf
+++ project_service.tf
@@ -4,3 +4,17 @@ resource "google_project_service" "cloudresourcemanager-googleapis-com" {
 
   disable_dependent_services = true
 }
+
+resource "google_project_service" "serviceusage-googleapis-com" {
+  project = google_project.master.project_id
+  service = "serviceusage.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "serviceusage-googleapis-com" {
+  project = google_project.master.project_id
+  service = "cloudaicompanion.googleapis.com"
+
+  disable_dependent_services = true
+}
