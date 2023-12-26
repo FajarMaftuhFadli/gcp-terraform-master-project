e2eef5ae371c49879e4d0d4716ac0fd4227cda93
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-26 17:46:14 +0700

    add testing SA

--- service_account.tf
+++ service_account.tf
@@ -3,3 +3,9 @@ resource "google_service_account" "service_account" {
   account_id   = "evros-oikolos"
   display_name = "Evros Oikolos"
 }
+
+resource "google_service_account" "service_account" {
+  project      = google_project.master.project_id
+  account_id   = "testing"
+  display_name = "testing"
+}
