ed325a1d003cf0f6b6c3d4647f8a4266416c4c4f
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-26 11:48:04 +0700

    Refactor project_service.tf for dynamic service configuration
    Refactor the Terraform file to use a local variable for defining
    multiple project services dynamically. This enhances maintainability and
    scalability. Additionally, it reduces code redundancy and allows for
    easier service additions or modifications in the future.

--- project_service.tf
+++ project_service.tf
@@ -1,20 +1,17 @@
-resource "google_project_service" "cloudresourcemanager-googleapis-com" {
-  project = google_project.master.project_id
-  service = "cloudresourcemanager.googleapis.com"
-
-  disable_dependent_services = true
+locals {
+  services = [
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "cloudaicompanion.googleapis.com"
+  ]
 }
 
-resource "google_project_service" "serviceusage-googleapis-com" {
-  project = google_project.master.project_id
-  service = "serviceusage.googleapis.com"
+resource "google_project_service" "services" {
+  for_each = toset(local.services)
 
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "cloudaicompanion-googleapis-com" {
   project = google_project.master.project_id
-  service = "cloudaicompanion.googleapis.com"
+  service = each.value
 
   disable_dependent_services = true
 }
