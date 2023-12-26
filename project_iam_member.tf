84a054b634a41534e4ae10f201347db3d60823f0
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-26 13:01:34 +0700

    Refactor project_iam_member.tf to utilize local roles Utilize local variables for project roles to enhance readability and maintainability of the code. This change simplifies management and future modifications.

--- project_iam_member.tf
+++ project_iam_member.tf
@@ -1,5 +1,14 @@
-resource "google_project_iam_member" "evros-oikolos-storage-objectAdmin" {
+locals {
+  evros-oikolos-roles = [
+    "roles/storage.admin",
+    "roles/iam.serviceAccountViewer"
+  ]
+}
+
+resource "google_project_iam_member" "evros-oikolos" {
+  for_each = toset(local.evros-oikolos-roles)
+
   project = google_project.master.project_id
-  role    = "roles/storage.objectAdmin"
+  role    = each.value
   member  = "serviceAccount:evros-oikolos@master-f44559.iam.gserviceaccount.com"
 }
