3f66fd6262211e7574e2786376c43809baf4dd3d
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-27 11:03:37 +0700

    Fix action and restore the tf

--- src/service_account.tf
+++ src/service_account.tf
@@ -3,9 +3,3 @@ resource "google_service_account" "service_account" {
   account_id   = "evros-oikolos"
   display_name = "Evros Oikolos"
 }
-
-resource "google_service_account" "service_account2" {
-  project      = google_project.master.project_id
-  account_id   = "evros-oikolos2"
-  display_name = "Evros Oikolos2"
-}
