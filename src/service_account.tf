0014f33f58acdafd38ee9216855907f2dbd17450
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-12-27 11:08:22 +0700

    Testing only

--- src/service_account.tf
+++ src/service_account.tf
@@ -3,3 +3,9 @@ resource "google_service_account" "service_account" {
   account_id   = "evros-oikolos"
   display_name = "Evros Oikolos"
 }
+
+resource "google_service_account" "service_account1" {
+  project      = google_project.master.project_id
+  account_id   = "evros-oikolos1"
+  display_name = "Evros Oikolos1"
+}
